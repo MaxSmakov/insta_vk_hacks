b9abe26ea1c87f780357b1b2bb5516e4104e08ad
MaxSmakov <maxsmakov@gmail.com>
2018-04-17 16:01:49 +0300

    add instagram hacks

--- main.js
+++ main.js
@@ -28,6 +28,7 @@ javascript:var cancel_invites=setTimeout(function cancel(){var a=document.queryS
 // Прокрутка страницы до самого низа с учетом подгрузки с сервера. Так же есть функция прокрутки до верха.
 // Usage:
 // Можно использовать, например во Вконтакте: на странице пользователя прокрутить его стену до самого низа.
+// открываем консоль (F12->console) и вставляем следующий код:
 
 var timerId = setTimeout(function tick() {
   var new_scroll_top = document.documentElement.scrollTop;            	
@@ -41,5 +42,57 @@ var timerId = setTimeout(function tick() {
 
 // прокрутка до самого верха страницы:
 window.scrollTo(0, 0);
+
+//END
+
+// START
+// Description:
+// Instagram. Ставим лайки всем подряд в ленте новостей (версия браузера естесственно)
+// Usage:
+// открываем консоль (F12->console) и вставляем следующий код:
+
+var n = 0;
+var k = 0;
+var timerId = setTimeout (function like(){
+	var len = document.querySelectorAll('._l9yih').length;
+	if ( document.querySelectorAll('._l9yih span')[len - 1].innerHTML == "Нравится") {
+		document.querySelectorAll('._l9yih')[len - 1].click();
+	}
+	window.scrollTo(0, n += 1000);
+	k++;
+	if (k > 30) { //не более 60 лайков в час, а то блокировка
+		clearTimeout(timerId);
+	} else {
+	timerId = setTimeout (like, 500);
+	}
+}, 500);
+
+//END
+
+// START
+// Description:
+// Instagram. Жестко лайкаем чью-нить страничку.
+// Usage:
+// Открываем нужный аккаунт, жмем на первую фотку, открываем консоль (F12->console) и вставляем следующий код:
+
+ var k = 0;
+ var timerId = setTimeout (function like() {	
+	if (k > 50 || (!document.querySelector('._3a693'))) { //не более 60 лайков в час, а то блокировка
+		clearTimeout(timerId);
+	} else {
+		if ( document.querySelector('._l9yih span').innerHTML == "Нравится") {
+ 			document.querySelector('._l9yih').click();//ставим лайк
+ 			k++; //увеличиваем счетчик
+ 		} 
+ 		document.querySelector('._3a693').click();//перелистываем
+		timerId = setTimeout (like, 1000 * Math.floor(Math.random() * 5 + 1));//для перестраховки интервал рандомный, можно настроить по-другому с любым интервалом
+	}
+}, 500);
+
+ if ( document.querySelector('._l9yih span').innerHTML == "Нравится") {
+ 	document.querySelector('._l9yih').click();//ставим лайк
+ } else {
+ 	document.querySelector('._3a693').click();//перелистываем
+ }
 
 //END
